59d2bfebf21c6998a1218ddf2826139ad35de607
Samuel Lujan <89878618+samupacino@users.noreply.github.com>
2026-09-03 22:24:19 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,51 @@
 import streamlit as st
-st.title("HOLA SAMUEL ESTAS DESDE STREAMLIT")
+
+ 
+
+st.title("Especialización Python for Analytics")
+
+st.sidebar.title("Parámetros")
+
+st.write("Elaborado por: Carlos Carrillo")
+
+ 
+
+modulos = st.sidebar.selectbox("Selecione el módulo",["Listas", "Arreglos", "Funciones", "POO"])
+
+ 
+
+if modulos == "Listas":
+
+  st.write("Te encuentras en el módulo de listas")
+
+ 
+
+  valor_inicial = int(st.number_input("Ingresa tu valor inicial del rango", value=0))
+
+  valor_final = int(st.number_input("Ingresa tu valor final del rango",value=10))
+
+ 
+
+  lista = list(range(valor_inicial, valor_final))
+
+ 
+
+  st.write(lista)
+
+ 
+
+elif modulos == "Arreglos":
+
+  st.write("Te encuentras en el módulo de arreglos")
+
+ 
+
+elif modulos == "Funciones":
+
+  st.write("Te encuentras en el módulo de Funciones")
+
+  
+
+else:
+
+  st.write("Te encuentras en el módulo de POO")
