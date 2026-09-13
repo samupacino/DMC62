2afe96686fed2148db62189f9a7b9cedb8f61fef
Samuel Lujan <89878618+samupacino@users.noreply.github.com>
2026-09-13 01:37:27 -0500

    Refactor module selection and improve UI elements

--- app.py
+++ app.py
@@ -1,51 +1,65 @@
 import streamlit as st
-
- 
+import numpy as np
+import pandas as pd
+import libreria_funciones as lf
 
 st.title("Especialización Python for Analytics")
-
 st.sidebar.title("Parámetros")
-
 st.write("Elaborado por: Carlos Carrillo")
 
- 
+st.image("Python_logo.png", width = 300)
+st.sidebar.image("DMC.png",width = 100)
 
-modulos = st.sidebar.selectbox("Selecione el módulo",["Listas", "Arreglos", "Funciones", "POO"])
 
- 
+modulos = st.sidebar.selectbox ("Selecione un módulo", ["Módulo Listas", "Módulo Arreglos", "Módulo Funciones", "Archivos"])
 
-if modulos == "Listas":
-
-  st.write("Te encuentras en el módulo de listas")
-
- 
-
-  valor_inicial = int(st.number_input("Ingresa tu valor inicial del rango", value=0))
-
-  valor_final = int(st.number_input("Ingresa tu valor final del rango",value=10))
-
- 
-
-  lista = list(range(valor_inicial, valor_final))
-
- 
-
-  st.write(lista)
-
- 
-
-elif modulos == "Arreglos":
-
-  st.write("Te encuentras en el módulo de arreglos")
-
- 
-
-elif modulos == "Funciones":
-
-  st.write("Te encuentras en el módulo de Funciones")
-
+if modulos == "Módulo Listas":
   
+  st.write("Bienvenido al módulo Listas")
+  
+  valor_inicial = st.number_input("Ingrese el valor inicial")
+  valor_final = st.number_input("Ingrese el valor final")
+  
+  lista_numeros = list(range(int(valor_inicial), int(valor_final)))
+  st.write(lista_numeros)
+  
+elif modulos == "Módulo Arreglos":
+  
+  st.write("Bienvenido al módulo de Arreglos")
+
+  cantidad_elementos = st.slider("Selecione la cantidad de elementos de su arreglo", 1,100)
+  cantidad_arreglo= np.arange(cantidad_elementos)
+  st.write(cantidad_arreglo)
+
+elif modulos == "Archivos":
+  
+  archivo = st.sidebar.file_uploader("Seleccione su archivo")
+
+  if archivo is not None:
+    st.write("Su archivo ha sido cargado")
+
+    if archivo.name.endswith(".csv"):
+      datos = pd.read_csv(archivo)
+      st.write(datos)
+    elif archivo.name.endswith(".xlsx"):
+      datos = pd.read_excel(archivo)
+      st.write(datos)
+
+  else:
+    st.write("Cargue su archivo")
+
+
+
+
 
 else:
+  
+  st.write("Bienvenido al módulo de Funciones")
 
-  st.write("Te encuentras en el módulo de POO")
+  capital_inicial = st.number_input("Capital inicial", min_value=0.0, value=1000.0)
+  tiempo_meses = st.number_input("Tiempo en meses", min_value=1, value=12)
+  tasa_porcentaje = st.number_input("Tasa de interés anual (%)", min_value=0.0, value=0.05)
+
+  resultado_interes_simple = lf.interes_simple(capital_inicial, tiempo_meses,tasa_porcentaje )
+  st.write(resultado_interes_simple)
+
